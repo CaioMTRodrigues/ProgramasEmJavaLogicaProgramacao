141637efff1e541aff2a483fe59bbc3714437544
CaioMTRodrigues <caiom.rodrigues@hotmail.com>
2024-09-28 16:35:32 -0300

    Algoritmo de Ordenação e Caixa Eletrônico

--- Alg04_OrdenacaoTresNumeros.java
+++ Alg04_OrdenacaoTresNumeros.java
@@ -1,3 +1,55 @@
+import java.util.Scanner;
+
 public class Alg04_OrdenacaoTresNumeros {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Solicitar três números ao usuário
+        System.out.print("Digite o primeiro número: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int num2 = input.nextInt();
+
+        System.out.print("Digite o terceiro número: ");
+        int num3 = input.nextInt();
+
+        // Variáveis para armazenar os números em ordem crescente
+        int menor, meio, maior;
+
+        // Comparar os números e organizá-los
+        if (num1 <= num2 && num1 <= num3) {
+            menor = num1;
+            if (num2 <= num3) {
+                meio = num2;
+                maior = num3;
+            } else {
+                meio = num3;
+                maior = num2;
+            }
+        } else if (num2 <= num1 && num2 <= num3) {
+            menor = num2;
+            if (num1 <= num3) {
+                meio = num1;
+                maior = num3;
+            } else {
+                meio = num3;
+                maior = num1;
+            }
+        } else {
+            menor = num3;
+            if (num1 <= num2) {
+                meio = num1;
+                maior = num2;
+            } else {
+                meio = num2;
+                maior = num1;
+            }
+        }
+
+        // Exibir os números em ordem crescente
+        System.out.println("Os números em ordem crescente são: " + menor + ", " + meio + ", " + maior);
+
+        input.close();
+    }
 }
--- Alg05_CaixaEletronico.java
+++ Alg05_CaixaEletronico.java
@@ -1,3 +1,50 @@
+import java.util.Scanner;
+
 public class Alg05_CaixaEletronico {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Solicita o valor desejado
+        System.out.print("Digite o valor a ser sacado: R$ ");
+        int valor = input.nextInt();
+
+        if (valor % 2 != 0 && valor % 5 != 0) {
+            System.out.println("Valor inválido. O valor deve ser múltiplo de 2 ou 5.");
+        } else {
+            // Contagem de cédulas
+            int notas200 = valor / 200;
+            valor %= 200;
+
+            int notas100 = valor / 100;
+            valor %= 100;
+
+            int notas50 = valor / 50;
+            valor %= 50;
+
+            int notas20 = valor / 20;
+            valor %= 20;
+
+            int notas10 = valor / 10;
+            valor %= 10;
+
+            int notas5 = valor / 5;
+            valor %= 5;
+
+            int notas2 = valor / 2;
+            valor %= 2;
+
+            // Exibir a quantidade de cada cédula
+            System.out.println("Cédulas fornecidas:");
+            if (notas200 > 0) System.out.println(notas200 + " nota(s) de R$200");
+            if (notas100 > 0) System.out.println(notas100 + " nota(s) de R$100");
+            if (notas50 > 0) System.out.println(notas50 + " nota(s) de R$50");
+            if (notas20 > 0) System.out.println(notas20 + " nota(s) de R$20");
+            if (notas10 > 0) System.out.println(notas10 + " nota(s) de R$10");
+            if (notas5 > 0) System.out.println(notas5 + " nota(s) de R$5");
+            if (notas2 > 0) System.out.println(notas2 + " nota(s) de R$2");
+        }
+
+        input.close();
+    }
 }
+
