6f779d15c96402cb177b4645a2e3ebef6b027b20
CaioMTRodrigues <caiom.rodrigues@hotmail.com>
2024-09-28 17:26:30 -0300

    Ordenação Vetor Decrescente

--- Alg10_OrdenacaoVetor.java
+++ Alg10_OrdenacaoVetor.java
@@ -1,3 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Collections;
+
 public class Alg10_OrdenacaoVetor {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        // Criar um vetor de 7 elementos
+        Integer[] vetor = new Integer[7];
+
+        // Receber os valores do usuário
+        System.out.println("Digite 7 números:");
+        for (int i = 0; i < 7; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            vetor[i] = input.nextInt();
+        }
+
+        // Ordenar o vetor em ordem decrescente
+        Arrays.sort(vetor,Collections.reverseOrder());
+
+        // Exibir o vetor ordenado
+        System.out.println("Vetor ordenado em ordem decrescente:");
+        for (int num : vetor) {
+            System.out.print(num + " ");
+        }
+
+        input.close();
+    }
 }
